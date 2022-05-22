ae0dd36efde5705cf96c50430101e4a26f8b467a
cokesebas <you@example.com>
2022-05-22 15:32:54 -0400

    Se añade información al archivo readme y se modifica el index para que responda correctemante al llamado de las apis o de la vista principal.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mantenedor-de-finanzas-Desafio_dentidesk
+
+# Crear e Instalar base de datos desde archivo 'desafio_dentidesk.sql' el cual se encontrará en la carpeta 'Base de Datos'
+
+# Iniciar proyecto desde la siguiente URL 'localhost/Mantenedor-de-finanzas-Desafio_dentidesk'
+
+# Proyecto realizado en PHP nativo bajo modelo vista controlador, se uso PHP 7.2 para realización de APIS
+
+# Para el front se utilizo html, css, bootstrap y vuejs
--- index.php
+++ index.php
@@ -4,37 +4,50 @@
 
     require PROJECT_ROOT_PATH . "/Controller/Api/movimientosController.php";
     
-
+    
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $uri = explode( '/', $uri );
+    $uri = explode('index.php',$uri);
 
-    $objFeedController = new movimientosController();
+    if(count($uri) == 1){
+        header('Location: Views/menu.php');
+    }elseif(count($uri) == 2){
+        $uri = explode('/',$uri[1]);
+
+        $objFeedController = new movimientosController();
 
 
-    if ((isset($uri[3]) && $uri[3] != 'movimientos') || !isset($uri[4])) {
+        if ((isset($uri[1]) && $uri[1] != 'movimientos') || !isset($uri[2])) {
+            header("HTTP/1.1 404 Not Found");
+            exit();
+        }
+
+        if($uri[2] == 'list'){
+            
+            $strMethodName = $uri[2] . 'Action';
+            $objFeedController->{$strMethodName}();
+        }
+
+        if($uri[2] == 'guardar'){
+            $strMethodName = $uri[2] . 'Action';
+            $objFeedController->{$strMethodName}();
+        }
+        
+        if($uri[2] == 'ganancias'){
+            $strMethodName = $uri[2] . 'Action';
+            $objFeedController->{$strMethodName}();
+        }
+
+
+        if($uri[2] == 'gananciasTotal'){
+            $strMethodName = $uri[2] . 'Action';
+            $objFeedController->{$strMethodName}();
+        }
+
+    }else{
         header("HTTP/1.1 404 Not Found");
         exit();
     }
 
-    if($uri[4] == 'list'){
-        
-        $strMethodName = $uri[4] . 'Action';
-        $objFeedController->{$strMethodName}();
-    }
+
     
-
-    if($uri[4] == 'guardar'){
-        $strMethodName = $uri[4] . 'Action';
-        $objFeedController->{$strMethodName}();
-    }
-    
-    if($uri[4] == 'ganancias'){
-        $strMethodName = $uri[4] . 'Action';
-        $objFeedController->{$strMethodName}();
-    }
-
-
-
-
-
 ?>
