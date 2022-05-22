a5206a73d4deacd96c2909361de0847cc099b430
cokesebas <you@example.com>
2022-05-21 22:56:21 -0400

    Se añaden modelos a usar

---
+++ Model/movimientosModel.php
@@ -0,0 +1,26 @@
+<?php
+require_once PROJECT_ROOT_PATH . "/Model/Database.php";
+ 
+class movimientosModel extends Database
+{
+    public function getAllMovimientos($limit){
+        return $this->select("SELECT * FROM movimientos LIMIT ?", ["i", $limit]);
+    }
+
+
+    public function insertMovimiento($datos){
+
+        $sql = "INSERT INTO movimientos (`ingreso`, `egreso`, `fecha`) VALUES (".$datos[0]['monto_ingreso'].", ".$datos[1]['monto_egreso'].", '".$datos[2]['fecha_transaccion']."'); ";
+        return $this->insert($sql);
+    }
+
+
+    public function getGananciasMes($datos){
+
+        $sql = 'SELECT (SUM(ingreso) - SUM(egreso)) AS ganancias FROM `movimientos` WHERE DATE_FORMAT(fecha, "%Y-%m") = "'.$datos[0]['fecha_transaccion'].'";';
+        return $this->select($sql);
+    }
+}
+
+
+?>
